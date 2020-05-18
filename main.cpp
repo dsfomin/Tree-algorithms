dc2986e991d543e8b5c7735af401c6e6553735bf
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-05-18 20:22:12 +0300

    Remove 'using namespace std' and 'system()' from the main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 
-using namespace std;
-
 int main() {
-    cout << "Hello, World!" << endl;
-	system("pause>nul");
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
